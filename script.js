86c4636fce8684ff4e20a6e3c00c9636975c2fc4
abdul bayees <bayees1@gmail.com>
2023-07-01 22:19:30 +0530

    hey

---
+++ script.js
@@ -0,0 +1,10 @@
+let productNameInputElement = document.getElementById("product-name");
+let remainingCharsElement = document.getElementById("remaining-chars");
+// let maxAllowedChars = productNameInputElement.maxLength;
+function updateRemainingChars(event){
+    let enteredText = event.target.value;
+    let enteredTextLength = enteredText.length;
+    let remainingCharacters = 60 - enteredTextLength
+    remainingCharsElement.textContent = remainingCharacters;  
+}
+productNameInputElement.addEventListener("input",updateRemainingChars);
