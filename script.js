aa69886ef125d368fa5d0ef828d2496e80ac9693
abdul bayees <bayees1@gmail.com>
2023-07-03 14:28:47 +0530

    added some extra features

--- script.js
+++ script.js
@@ -6,5 +6,109 @@ function updateRemainingChars(event){
     let enteredTextLength = enteredText.length;
     let remainingCharacters = 60 - enteredTextLength
     remainingCharsElement.textContent = remainingCharacters;  
+    if (remainingCharacters ===0) {
+        productNameInputElement.classList.add("error")
+        remainingCharsElement.classList.add("error")
+    }
+    else if(remainingCharacters <=10){
+        productNameInputElement.classList.add("warning")
+        remainingCharsElement.classList.add("warning")
+   
+    }
+    else{
+        productNameInputElement.classList.remove("warning","error")
+        remainingCharsElement.classList.remove("warning","error")
+
+    }
 }
 productNameInputElement.addEventListener("input",updateRemainingChars);
+remainingChars = document.getElementById("product-name")
+
+
+// let username = "abdul"
+// if (username=="abdul"){
+//     console.log('User name is abdul')
+// }
+//     else{
+//         console.log("user is not logged in")
+//     }
+
+//for loop
+// for (let i = 0; i < 100; i++) {
+//     console.log(i)
+    
+// }
+/// for of loop
+// let users = ["abdul","bayees","abuhusaina"]
+// for (let user of users) {
+//     console.log(user)
+// }
+
+///for in loop
+// const LoggedinUSer = {
+//     name:"abdul bayees",
+//     age:32,
+//     isAdmin:true
+// }
+// for (const propertyName in LoggedinUSer) {
+//     console.log(propertyName)
+
+
+
+//While Loop
+// let isFininshed = false
+// while (!isFininshed) {
+//     isFininshed= confirm("Do You Really wanna see this website")
+// }
+// console.log("User Agreed")
+
+let calculateBtn = document.querySelector("#calculate-sum button");
+let display = document.querySelector("#calculate-sum .display");
+
+calculateBtn.addEventListener("click", calculateSum);
+
+function calculateSum() {
+  let numberInput = document.getElementById("num");
+  let enteredNumber = parseInt(numberInput.value);
+  let sumUp = 0;
+
+  for (let i = 1; i <= enteredNumber; i++) {
+    sumUp += i;
+  }
+
+  display.textContent = "Sum: " + sumUp;
+  display.style.display = "block";
+}
+
+let highlightBtn = document.querySelector("#btn-highlight")
+highlightBtn.addEventListener("click",changeColor)
+function changeColor(){ 
+    const highlightTexts = document.querySelectorAll("#highlight strong")
+  for (const highlightText of highlightTexts) {
+    highlightText.classList.add("strongAll")
+         
+          
+      }
+  }
+
+  let displayBtn = document.querySelector("#display-btn");
+  displayBtn.addEventListener("click", showDetails);
+  
+  function showDetails() {
+    let outputDataElement = document.querySelector("#show-details");
+    outputDataElement.innerHTML = "";
+  
+    const details = {
+      name: "Abdul Bayees",
+      age: 20,
+      occupation: "Software Engineer",
+    };
+  
+    for (const detail in details) {
+      let newUser = document.createElement("li");
+      let outputText = detail.toUpperCase() + ": " + details[detail];
+      newUser.textContent = outputText;
+      outputDataElement.append(newUser);
+    }
+  }
+  
